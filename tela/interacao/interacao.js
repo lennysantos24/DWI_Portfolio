65c3e26f7dcfc3d77df7dcff2592b930c5c6d3cc
Milene <mimilene2018@gmail.com>
2024-05-10 14:42:59 -0300

    header
    Ainda precisa ajustar a borda

--- tela/interacao/interacao.js
+++ tela/interacao/interacao.js
@@ -1,3 +1,7 @@
+
+
+// MENU JAVASCRIPT
+
 const buttons = document.querySelectorAll(".nav-menu-item");
 
 function handleButton() {
@@ -7,3 +11,41 @@ function handleButton() {
 }
 
 buttons.forEach((item) => item.addEventListener("pointerdown", handleButton));
+
+//CABECALHO TEXTO
+
+var div = document.getElementById('Ola');
+var texto = 'Olá!';
+
+function escrever(str, el) {
+  var char = str.split('').reverse();
+  var typer = setInterval(function() {
+    if (!char.length) return clearInterval(typer);
+    var next = char.pop();
+    el.innerHTML += next;
+  }, 250);
+}
+
+escrever(texto, div)
+
+var nome = document.getElementById('nome');
+var eu = 'Meu nome é Milene Santos :D';
+
+setTimeout(function() {
+  function escrever(str, el) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+      if (!char.length) return clearInterval(typer);
+      var next = char.pop();
+      el.innerHTML += next;
+    }, 100);
+  }
+  escrever(eu, nome)}, 1500);
+
+  var div = document.getElementById('breve_descricao');
+  div.style.display = 'none';
+  
+
+  setTimeout(function() {
+      div.style.display = 'block';
+  }, 4700);
